eaf115d566c8c9654e5f75e1cf2a1f5aec9e0a3a
Babak Hemmatian <babak.hemmatian@gmail.com>
2026-04-03 17:13:51 -0500

    Refactor constResponses to use constants for keys

--- server/config/constResponse.js
+++ server/config/constResponse.js
@@ -1,5 +1,5 @@
 export const constResponses = {
-    'un libro o libros (usados para leer)': {
+    BOOK: {
         'high':[
             "construir una casa para niños",
             "regla",
@@ -54,7 +54,7 @@ export const constResponses = {
         ]
     },
 
-    'un tenedor o tenedores (usados para comer)':{
+    FORK: {
         'high':[
             "Si pones una banda elástica alrededor de los dientes de un tenedor se convierte en un mini trampolín para hormigas u otros insectos pequeños",
             "Mantener tu bebida fría congelando algunos tenedores y agregándolos al vaso",
@@ -108,7 +108,7 @@ export const constResponses = {
         ]
     },
 
-    'una lata o latas de metal (usadas para almacenar alimentos perecederos)':{
+    CAN: {
         'high':[
             "Usar la pestaña de una lata para hacer ropa",
             "imprenta",
